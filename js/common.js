78ea779c1ff37a165447dfaaeb318256f9ca9e11
tatynechka <tatynechka@mail.ru>
2016-05-19 01:37:56 +0300

    Разметка Главной

--- js/common.js
+++ js/common.js
@@ -9,4 +9,32 @@ $(function() {
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
+	//Форма заказа в модальном окне
+	$('.callme-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+
+});
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > 1){
+		$('header').addClass("sticky");
+	}
+	else{
+		$('header').removeClass("sticky");
+	}
 });

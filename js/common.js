dfad828f3431e67aca14909e99e6f4ffa35b941d
tatynechka <tatynechka@mail.ru>
2016-06-22 01:48:17 +0300

    Портфолио

--- js/common.js
+++ js/common.js
@@ -104,6 +104,50 @@ $(function() {
 		}
 	});
 
+	$('.popup-gallery-2').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+  			markup: '<div class="mfp-figure">'+
+	            '<div class="mfp-close"></div>'+
+	            '<div class="mfp-img"></div>'+
+	            '<div class="mfp-bottom-bar">'+
+	              '<div class="mfp-title"></div>'+
+	              '<div class="mfp-counter"></div>'+
+	            '</div>'+
+	          '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
+
+  			cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
+
+		    titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
+		    // Or the function that should return the title. For example:
+		    // titleSrc: function(item) {
+		    //   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+		    // }
+
+		    verticalFit: true, // Fits image in area vertically
+
+		    tError: '<a href="%url%">The image</a> could not be loaded.' // Error message
+		}
+
+	});
+
+	//Портфолио
+	$('.grid').masonry({
+	  // options
+	  itemSelector: '.grid-item',
+	  columnWidth: '.persent-size',
+      percentPosition: true,
+      resize: false
+	});
+
 
 	//при наведении на иконку преимуществ, подсвечиваем ссылку
 	$('.advantages-work--icon').mouseover(function(){
